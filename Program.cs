92af90d5ac8aa9ba1545a6bc5645dd0a9596f3a1
Sakshi03022004 <99466592+Sakshi03022004@users.noreply.github.com>
2023-04-23 19:10:28 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,50 @@
-﻿using System;
+﻿//program to demonstarte number is perfect or not
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace _1_hello_word
+namespace perfectno
 {
-    class Program
+
+    class demo
+    {
+        int no, r, sum = 0;
+        public void read()
+        {
+            Console.WriteLine("enter the number");
+            no = Convert.ToInt32(Console.ReadLine());
+            r = 1;
+        }
+        public void show()
+        {
+            while (r < no)
+            {
+                if (no % r == 0)
+                {
+                    sum = sum + r;
+                }
+              r= r + 1;
+            }
+            if (no == sum)
+            {
+                Console.WriteLine("number is perfect");
+            }
+            else
+            {
+                Console.WriteLine("number is not perfect");
+            }
+        }
+    }
+
+    class perfectno
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 10; i++)
-            {
-                Console.WriteLine("hello word");
-                
-            }
+            demo d1 = new demo();
+            d1.read();
+            d1.show();
             Console.ReadLine();
         }
-        
     }
 }
